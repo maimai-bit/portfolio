1c65aa454347ffa0874980973344c47ec7be20b1
maimai-bit <maikellyjordan05@gmail.com>
2025-06-10 12:17:01 +0300

    polishing flow

---
+++ global/global.js
@@ -0,0 +1,8 @@
+function confirmEmailRedirect(e) {
+    e.preventDefault(); 
+    const confirmed = confirm("You are being redirected to your email. Proceed?");
+    if (confirmed) {
+      
+      window.location.href = "mailto:maikellyjordan@gmail.com?subject=Contact&body=Hi%20Maikelly%2C";
+    }
+  }
