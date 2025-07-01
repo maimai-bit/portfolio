db3f24d944a68aefb84882e521fe2b9efb51a03b
maimai-bit <maikellyjordan05@gmail.com>
2025-07-01 17:06:53 +0400

    fixing js issue

--- global/global.js
+++ global/global.js
@@ -7,28 +7,4 @@ function confirmEmailRedirect(e) {
     }
   }
 
-  // carrossel //
-
-  const track = document.querySelector('.carousel-track');
-const slides = Array.from(track.children);
-const nextBtn = document.querySelector('.carousel-btn.next');
-const prevBtn = document.querySelector('.carousel-btn.prev');
-let currentSlide = 0;
-
-function updateCarousel() {
-  const slideWidth = slides[0].getBoundingClientRect().width;
-  track.style.transform = `translateX(-${slideWidth * currentSlide}px)`;
-}
-
-nextBtn.addEventListener('click', () => {
-  currentSlide = (currentSlide + 1) % slides.length;
-  updateCarousel();
-});
-
-prevBtn.addEventListener('click', () => {
-  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-  updateCarousel();
-});
-
-window.addEventListener('resize', updateCarousel);
-updateCarousel(); // Initial position
+  
